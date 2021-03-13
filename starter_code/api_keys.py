902534c43067f5593bbad4ba12e071c1f3961edf
Tejas Patel <Tejas@Tejass-MacBook-Pro.local>
2021-03-13 10:36:27 -0500

    HW-06

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "b673256a3f8f054b0d4b7e3cfb6eede8"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyBesHrKtkad6io2p5AKysSe424Tmrj_zYE"
+g_key = ""
